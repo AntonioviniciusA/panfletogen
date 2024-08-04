70484d39a0a72d9b03e3f67b1c7fefa5f7e0fb8b
AntonioviniciusA <antoniovinicius_@outlook.com>
2024-08-04 20:03:51 -0300

    app /

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CardsPage from "./components/CardsPage";
+import FormPage from "./components/FormPage";
 import "./style.css";
 
 function App() {
