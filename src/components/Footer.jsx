219a0793373e450157c45d3f8689d1629f9700a3
AntonioviniciusA <antoniovinicius_@outlook.com>
2024-08-07 14:58:57 -0300

    footer add: mail and tel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,12 @@ const Footer = () => {
   return;
   <div>
     <div>
+      <a href="mailto:antoniovinicius_@outlook.com">
+        antoniovinicius_@outlook.com
+      </a>
+      <a href="tel:+55983394560">+55(61)983394560</a>
+    </div>
+    <div>
       <img src="" alt="" />
     </div>
   </div>;
