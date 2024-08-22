cd69f42291ac654106790b1d5474650e6763a386
AntonioviniciusA <antoniovinicius_@outlook.com>
2024-08-22 17:43:14 -0300

    login add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../output.css";
 import logodarkctitulo from "../img/logo dark c titulo.png";
 import netlifyIdentity from "netlify-identity-widget";
 
 const Header = () => {
+  const [user, setUser] = useState(null);
   useEffect(() => {
     netlifyIdentity.init();
+    netlifyIdentity.on("login", (user) => {
+      setUser(user);
+    });
+    netlifyIdentity.on("logout", () => {
+      setUser(null);
+    });
   }, []);
 
   const handleLogin = () => {
     netlifyIdentity.open();
   };
+
+  const handleLogout = () => {
+    netlifyIdentity.logout();
+  };
+
   return (
     <div className="bg-color3 flex gap-5 flex-col">
       <div className="bg-color2 flex flex-row xl:justify-between items-center w-full">
@@ -20,18 +32,28 @@ const Header = () => {
           </a>
         </div>
         <div>
-          <button
-            onClick={handleLogin}
-            className="bg-color1  m-2  gugi-regular px-3 text-white rounded-xl"
-          >
-            LOGIN
-          </button>
-          <button
-            onClick=""
-            className="bg-color1 px-3 gugi-regular m-2 text-white rounded-xl"
-          >
-            RESGISTRAR
-          </button>
+          {user ? (
+            <>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout} className="logout-button">
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <li>
+              {" "}
+              <button
+                onClick={handleLogin}
+                className="bg-color1  m-2  gugi-regular px-3 text-white rounded-xl"
+              >
+                LOGIN
+              </button>
+            </li>
+          )}
         </div>
       </div>
     </div>
