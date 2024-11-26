7a8a59702f4697689eb41dd0e8608de85ade60b7
AntonioviniciusA <antoniovinicius_@outlook.com>
2024-11-26 16:24:29 -0300

    att

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,65 @@
 import React from "react";
-import "../output.css";
-import tel from "../img/whatsapplogo.png";
-import emaillogo from "../img/emaillogo.png";
-import logodark from "../img/logo dark.png";
+import "../style/footer.css";
 
 const Footer = () => {
   return (
-    <div className="bg-color2 gap-2 flex justify-between">
-      <div className="flex items-center justify-start text-white">
-        <img src={emaillogo} className="logo" alt="icone telefone" />
-        <a href="mailto:antoniovinicius_@outlook.co">antonio vinicius</a>
-        <br />
-        <img src={tel} alt="icone telefone" className="logo" />
-        <a href="tel:+55983394560">+55(61)983394550</a>
+    <footer id="footer">
+      <div class="footer-container">
+        <div class="footer-section">
+          <h3 class="footer-title">Entre em contato</h3>
+          <p class="footer-text">📍 Rua Exemplo, 123 - Centro</p>
+          <p class="footer-text">📞 (11) 98765-4321</p>
+          <p class="footer-text">📧 contato@exemplo.com</p>
+        </div>
+
+        <div class="footer-section">
+          <h3 class="footer-title">Links rápidos</h3>
+          <ul class="footer-links">
+            <li>
+              <a href="/sobre" class="footer-link">
+                Sobre nós
+              </a>
+            </li>
+            <li>
+              <a href="/servicos" class="footer-link">
+                Serviços
+              </a>
+            </li>
+            <li>
+              <a href="/contato" class="footer-link">
+                Fale conosco
+              </a>
+            </li>
+            <li>
+              <a href="/politica" class="footer-link">
+                Política de privacidade
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="footer-section">
+          <h3 class="footer-title">Siga-nos</h3>
+          <div class="footer-social">
+            <a href="https://facebook.com" target="_blank" class="footer-link">
+              Facebook
+            </a>
+            <a href="https://instagram.com" target="_blank" class="footer-link">
+              Instagram
+            </a>
+            <a href="https://linkedin.com" target="_blank" class="footer-link">
+              LinkedIn
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="">
-        <img src={logodark} className="logo" alt="logo" />
+
+      <div class="footer-bottom">
+        <p class="footer-bottom-text">
+          © 2024 Panfletos Exemplo. Todos os direitos reservados.
+        </p>
       </div>
-      <div className="opacity-0  right-3">
-        <p>&&</p>
-      </div>
-    </div>
+    </footer>
   );
 };
 export default Footer;
